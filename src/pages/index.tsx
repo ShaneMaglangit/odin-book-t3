944ea1f76d64de6091d6680452d89c88ca9354f1
ShaneMaglangit <shanedmaglangit@gmail.com>
2022-09-11 00:36:55 +0800

    Add post box placeholder

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
 import type {NextPage} from 'next'
 import Head from 'next/head'
 import {requireAuthentication} from '../server/common/requireAuthentication'
-import {signOut} from 'next-auth/react'
 import Navbar from '../components/navbar'
+import PostBox from '../components/postbox'
 
-const Home: NextPage = () => {
+const Index: NextPage = () => {
     return (
         <>
             <Head>
@@ -14,41 +14,16 @@ const Home: NextPage = () => {
             </Head>
 
             <Navbar/>
-            <main className="container mx-auto flex flex-col items-center justify-center min-h-screen p-4">
-
+            <main className="bg-gray-50 dark:bg-gray-600">
+                <div
+                    className=" container mx-auto max-w-7xl flex flex-col items-center justify-start min-h-screen py-4 px-8">
+                    <PostBox/>
+                </div>
             </main>
         </>
     )
 }
 
-export default Home
-
-type TechnologyCardProps = {
-    name: string;
-    description: string;
-    documentation: string;
-};
-
-const TechnologyCard = ({
-                            name,
-                            description,
-                            documentation,
-                        }: TechnologyCardProps) => {
-    return (
-        <section
-            className="flex flex-col justify-center p-6 duration-500 border-2 border-gray-500 rounded shadow-xl motion-safe:hover:scale-105">
-            <h2 className="text-lg text-gray-700">{name}</h2>
-            <p className="text-sm text-gray-600">{description}</p>
-            <a
-                className="mt-3 text-sm underline text-violet-500 decoration-dotted underline-offset-2"
-                href={documentation}
-                target="_blank"
-                rel="noreferrer"
-            >
-                Documentation
-            </a>
-        </section>
-    )
-}
-
 export const getServerSideProps = requireAuthentication()
+
+export default Index
