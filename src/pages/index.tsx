ad3ab05cd68808fe0163bea946a52fb4f6fb17b2
ShaneMaglangit <shanedmaglangit@gmail.com>
2022-09-11 00:19:49 +0800

    Add navbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import type {NextPage} from 'next'
 import Head from 'next/head'
 import {requireAuthentication} from '../server/common/requireAuthentication'
 import {signOut} from 'next-auth/react'
+import Navbar from '../components/navbar'
 
 const Home: NextPage = () => {
     return (
@@ -12,58 +13,9 @@ const Home: NextPage = () => {
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
 
+            <Navbar/>
             <main className="container mx-auto flex flex-col items-center justify-center min-h-screen p-4">
-                <h1 className="text-5xl md:text-[5rem] leading-normal font-extrabold text-gray-700">
-                    Create <span className="text-purple-300">T3</span> App
-                </h1>
-                <p className="text-2xl text-gray-700">This stack uses:</p>
-                <div className="grid gap-3 pt-3 mt-3 text-center md:grid-cols-3 lg:w-2/3">
-                    <TechnologyCard
-                        name="NextJS"
-                        description="The React framework for production"
-                        documentation="https://nextjs.org/"
-                    />
-                    <TechnologyCard
-                        name="TypeScript"
-                        description="Strongly typed programming language that builds on JavaScript, giving you better tooling at any scale"
-                        documentation="https://www.typescriptlang.org/"
-                    />
-                    <TechnologyCard
-                        name="TailwindCSS"
-                        description="Rapidly build modern websites without ever leaving your HTML"
-                        documentation="https://tailwindcss.com/"
-                    />
-                    <TechnologyCard
-                        name="tRPC"
-                        description="End-to-end typesafe APIs made easy"
-                        documentation="https://trpc.io/"
-                    />
-                    <TechnologyCard
-                        name="Next-Auth"
-                        description="Authentication for Next.js"
-                        documentation="https://next-auth.js.org/"
-                    />
-                    <TechnologyCard
-                        name="Prisma"
-                        description="Build data-driven JavaScript & TypeScript apps in less time"
-                        documentation="https://www.prisma.io/docs/"
-                    />
-                </div>
 
-                <button type="button"
-                        onClick={() => signOut()}
-                        className="mt-8 group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                        <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-                            <svg className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
-                                 xmlns="http://www.w3.org/2000/svg"
-                                 viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                <path fillRule="evenodd"
-                                      d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z"
-                                      clipRule="evenodd"/>
-                            </svg>
-                        </span>
-                    Sign out
-                </button>
             </main>
         </>
     )
